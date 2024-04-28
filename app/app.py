89b4a3e816895ef08f55352dc0562e1dc368ad1a
nbalint99 <79223159+nbalint99@users.noreply.github.com>
2024-04-28 14:24:50 +0200

    Init form

--- app/app.py
+++ app/app.py
@@ -8,7 +8,7 @@ def health():
 
 @app.route("/")
 def hello_world():
-    return "Can you see this text? I hope so!"
+    return app.send_static_file('index.html')
 
 if __name__ == '__main__':
    app.run(host="0.0.0.0", port=5000)
