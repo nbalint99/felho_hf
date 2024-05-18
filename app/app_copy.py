5438207c52d90d6c1b9b96e6ed16c1c1ab199694
nbalint99 <79223159+nbalint99@users.noreply.github.com>
2024-05-18 15:19:15 +0200

    asd

---
+++ app/app_copy.py
@@ -0,0 +1,37 @@
+from flask import Flask, request, redirect, url_for, render_template, send_from_directory
+import os
+
+app = Flask(__name__)
+UPLOAD_FOLDER = "uploads"
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
+
+@app.route("/health")
+def health():
+    return "OK"
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/upload", methods=["POST"])
+def upload_photo():
+    if "file" not in request.files:
+        return "No file"
+    file = request.files['file']
+    if file.filename == "":
+       return "No selected"
+    if file:
+       description = request.form["description"]
+       pathPhoto = os.path.join(app.config["UPLOAD_FOLDER"], file.filename)
+       file.save(pathPhoto)
+       return render_template("upload.html", filename=file.filename, description=description)
+
+@app.route("/uploads/<filename>")
+def uploaded_photo(filename):
+    return send_from_directory(app.config["UPLOAD_FOLDER"], filename)
+
+if __name__ == '__main__':
+   app.run(host="0.0.0.0", port=5000)
