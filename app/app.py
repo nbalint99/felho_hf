c6ca5597dcd3cb4f7ae10e18bbe7664d3b2ccb7d
nbalint99 <79223159+nbalint99@users.noreply.github.com>
2024-05-18 13:29:38 +0200

    test3

--- app/app.py
+++ app/app.py
@@ -1,14 +1,37 @@
-from flask import Flask
+from flask import Flask, request, redirect, uré_for, render_template, send_form_directory
+import os
 
 app = Flask(__name__)
+UPLOAD_FOLDER = "uploads"
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
 
 @app.route("/health")
 def health():
     return "OK"
 
 @app.route("/")
-def hello_world():
-    return "Hello, World!!"
+def index():
+    return render_template("index.html")
 
-if __name__ == '__main__':  
+@app.route("/upload", methods=["POST"])
+def upload_photo():
+    if "file" not in request.files:
+        return "No file"
+    file = request.files['file']
+    if file.filename == "":
+       return "No selected"
+    if file:
+       description = request.form["description"]
+       pathPhoto = os.path.join(app.config["UPLOAD_FOLDER"], file.filename)
+       file.save(pathPhoto)
+       return render_template("upload.html", filename=file.filename, description=description)
+
+@app.route("/uploads/<filename>")
+def uploaded_photo(filename):
+    return send_from_directory(app.config["UPLOAD_FOLDER"], filename)
+
+if __name__ == '__main__':
    app.run(host="0.0.0.0", port=5000)
