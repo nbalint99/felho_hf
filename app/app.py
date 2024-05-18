464650f97fc4f74ded076753452f53103b3a8909
nbalint99 <79223159+nbalint99@users.noreply.github.com>
2024-05-18 22:05:54 +0200

    messeage

--- app/app.py
+++ app/app.py
@@ -2,13 +2,13 @@ from flask import Flask, request, redirect, url_for, render_template, jsonify
 from flask_pymongo import PyMongo
 import os
 
-app = Flask(__name__, template_folder='app/templates')
+app = Flask(__name__, template_folder='templates')
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI", "mongodb://mongo:27017/carspics")
 mongo = PyMongo(app)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template('index.html')
 
 @app.route('/health')
 def health():
