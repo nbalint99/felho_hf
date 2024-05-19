0aa4f3818b59ce0cfe72368f580b25b8b7e3790d
nbalint99 <79223159+nbalint99@users.noreply.github.com>
2024-05-19 11:09:48 +0200

    Add GridFS

--- app/app.py
+++ app/app.py
@@ -1,10 +1,13 @@
 from flask import Flask, request, redirect, url_for, render_template, jsonify
-from flask_pymongo import PyMongo
+from flask_pymongo import PyMongo, ObjectId
+from gridfs import GridFS
+from bson import ObjectId
 import os
 
 app = Flask(__name__, template_folder='templates')
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI", "mongodb://mongo:27017/carspics")
 mongo = PyMongo(app)
+fs = GridFS(mongo.db)
 
 @app.route("/")
 def index():
@@ -23,18 +26,25 @@ def upload_file():
         return "No file selected"
     if file:
         description = request.form["description"]
-        mongo.save_file(file.filename, file)
-        mongo.db.uploads.insert_one({"filename": file.filename, "description": description})
-        return redirect(url_for("uploaded_file", filename=file.filename))
+        file_id = fs.put(file, filename=file.filename, content_type=file.content_type, description = description)
+        return redirect(url_for('uploads', file_id=file_id))
+
+@app.route("/file/<file_id>")
+def file(file_id):
+    try:
+        file = fs.get(ObjectId(file_id))
+        return send_file(file, mimetype=file.content_type)
+    except Exception as exc:
+        return str(exc)
 
 @app.route("/uploads/<filename>")
-def uploaded_file(filename):
-    upload = mongo.db.uploads.find_one_or_404({"filename": filename})
-    return render_template("upload.html", filename=filename, description=upload["description"])
-
-@app.route("/file/<filename>")
-def file(filename):
-    return mongo.send_file(filename)
+def uploads(file_id):
+    try:
+        file = fs.get(ObjectId(file_id))
+        description = file.description
+        return render_template("upload.html", filename=file.filename, description=description, file_id=file_id)
+    except Exception as exc:
+        return str(exc)
 
 if __name__ == "__main__":
    app.run(host="0.0.0.0", debug=True)
